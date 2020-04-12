1d03c617284d00f6d4ebee9c4d861ed18a23613b
Ice1779 <50962640+Ice1779@users.noreply.github.com>
2020-04-12 18:36:15 +0530

    Add files via upload

---
+++ randmeme.py
@@ -0,0 +1,21 @@
+import requests
+global mainurl
+mainurl="https://meme-api.herokuapp.com/gimme"
+class Gimme_Instance:
+    def __init__(self,subreddit='',warning=True): #generates api instance and gets data into variables
+        if warning:
+            if subreddit!='':
+                print("We observe that you are not using the default subreddits. This might result in the retrieval of a reddit post instead of a meme depending on the subreddit.\n")
+        try:
+            r=requests.get(mainurl+'/'+str(subreddit))
+            if r.status_code==200:
+                data=r.json()
+                self.postlink=str(data['postLink'])
+                self.subreddit=str(data['subreddit'])
+                self.title=str(data['title'])
+                self.img_url=str(data['url'])
+
+        except:
+            print("Invalid subreddit")
+
+x=Gimme_Instance()
